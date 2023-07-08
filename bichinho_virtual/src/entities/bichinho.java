b8949c129b96444deb062c24fa05a53a5f422e19
carolmedici <carolina.obstetriz@gmail.com>
2023-07-08 20:50:36 -0300

    primeiro commit - iniciando projeto

---
+++ bichinho_virtual/src/entities/bichinho.java
@@ -0,0 +1,12 @@
+package entities;
+
+public class bichinho {
+	
+	private String nome;
+	private String tipo;
+	private int idade;
+	private boolean estado;
+	private int caloria;
+	private int forca;
+
+}
